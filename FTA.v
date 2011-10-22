bd93d1d59ffe650719f2ace581279e332a62f87a
Patrick Hulin <patrick.hulin@gmail.com>
2011-10-22 18:20:08 -0400

    working euclids lemma, prime factorization lemma

--- FTA.v
+++ FTA.v
@@ -1,7 +1,7 @@
 Require Import CpdtTactics.
 Require Import List.
+Require Import ZArith.
 Require Import Znumtheory.
-Require Import BinInt.
 Require Import Classical.
 
 Ltac destruct_div :=
@@ -21,8 +21,7 @@ Inductive prod_primes : Z -> Prop :=
 Hint Constructors prod_primes.
 Hint Resolve prime_alt.
 
-
-Lemma not_prime_divisible : forall z, z > 1 -> ~ (prime z) -> exists x, x < z /\ (x | z).
+Lemma not_prime_divisible : forall z, z > 1 -> ~ (prime z) -> exists x, 1 < x < z /\ (x | z).
   intros.
   assert (~ prime' z).
   destruct (prime_alt z).
@@ -37,7 +36,7 @@ Lemma not_prime_divisible : forall z, z > 1 -> ~ (prime z) -> exists x, x < z /\
   apply NNPP in H3.
   exists x.
   crush.
-Qed.  
+Qed.
 
 Hint Resolve not_prime_divisible.
 
@@ -47,12 +46,56 @@ Qed.
 
 Hint Resolve prime_or_divisible.
 
-Theorem all_prod_primes : forall n, n > 2 -> prod_primes n.
-  assert (forall k n, n <= k -> n > 2 -> prod_primes n).
-  apply (Zind (fun k => (forall n, n <= k -> n > 2 -> prod_primes n)));  crush.
-  
-  
-  
+Hint Resolve Zdivide_Zdiv_eq.
+
+(* what a piece of shit... *)
+Theorem all_prod_primes : forall n, n > 1 -> prod_primes n.
+  assert (forall k n, n <= k -> n > 1 -> prod_primes n).
+  apply (Zind (fun k => (forall n, n <= k -> n > 1 -> prod_primes n))); crush.
+  rewrite <- Zsucc_succ' in H0.
+  destruct (prime_dec n).
+  crush.
+  assert (exists z, 1 < z < n /\ (z | n)).
+  apply not_prime_divisible; crush.
+  destruct H2.
+  destruct H2.
+  apply (recprime n (Zabs x0) (n / (Zabs x0))); crush.
+  rewrite Zmult_comm.
+  apply Zdivide_Zdiv_eq.
+  assert (x0 <> 0).
+  intro.
+  subst.
+  assert (n | 0) by crush.
+  assert (n = 0 \/ n = -0) by (apply Zdivide_antisym; assumption).
+  crush.
+  assert (Zabs x0 <> 0).
+  pose (Zabs_eq_case x0 0); crush.
+  pose (Zabs_pos x0); crush.
+  apply Zdivide_Zabs_inv_l; assumption; crush.
+  rewrite Zabs_eq.
+  apply (H x0); crush.
+  apply (Zlt_le_trans x0 n (Zsucc x)) in H0; crush.
+  rewrite Zabs_eq.
+  assert (0 < n / x0 < n).
+  apply Zdivide_Zdiv_lt_pos; crush.
+  apply H.
+  crush.
+  assert (n / x0 <> 1).
+  intro.
+  assert (n = x0).
+  rewrite <- (Zmult_1_r x0).
+  rewrite <- H6.
+  apply Zdivide_Zdiv_eq; crush.
+  crush.
+  crush.
+  crush.
+  rewrite <- (Zpred_pred') in H0.
+  pose (Zle_pred x).
+  apply (Zle_trans n (Zpred x) x) in z; crush.
+  intros.
+  specialize (H (Zsucc n) n).
+  crush.
+Qed.
 
 (* this proof is a piece of shit. *)
 Theorem euclids_lemma : forall p a b, prime p -> (p | a * b) -> (p | a) \/ (p | b).
@@ -67,6 +110,13 @@ Theorem euclids_lemma : forall p a b, prime p -> (p | a * b) -> (p | a) \/ (p | 
   apply Zmult_1_l.
   rewrite <- (Zmult_assoc v a b) in H2.
   rewrite H0 in H2.
+  (*match goal with
+    | [ H : context [ p ] |- _ ] => match H with
+                                      | context [ p * ?m ] => pose 1 (*rewrite (Zmult_comm p m) in H *)
+                                      
+                                      | _ => pose H
+                                    end
+  end.*)
   rewrite (Zmult_assoc v q p) in H2.
   rewrite <- (Zmult_assoc u p b) in H2.
   rewrite (Zmult_comm p b) in H2.
@@ -77,7 +127,7 @@ Theorem euclids_lemma : forall p a b, prime p -> (p | a * b) -> (p | a) \/ (p | 
   auto.
 Qed.
 
-Theorem fundamental_theorem_of_arithmetic : 
+
 
 (*Inductive divides : nat -> nat -> Prop :=
   div : forall a b, (exists x, b = a * x) -> divides a b.
